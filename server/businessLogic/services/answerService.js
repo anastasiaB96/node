2de2d8608b304fc90b1f6b32d5c6d524951813c1
nastassia.baranava <nastassia.baranava@itechart-group.com>
2018-05-16 16:54:36 +0300

    v1 - added answer find mapper

--- server/businessLogic/services/answerService.js
+++ server/businessLogic/services/answerService.js
@@ -1,12 +1,31 @@
 'use strict';
 
 import AuditableService from './auditableService';
+import * as answerDALtoDTO from '../models/answerDALtoDTO.json';
 
 export default class AnswerService extends AuditableService {
   constructor(errorsHelper, logger, mapper, answerRepository, answerVoteService) {
     super({ errorsHelper, logger, mapper, repository: answerRepository });
 
     this.answerVoteService = answerVoteService;
+  }
+
+  async findAll() {
+    const result = await super.findAll();
+
+    this.resolve(result.map(answer => this.mapper.mapObject(answer, answerDALtoDTO)));
+  }
+
+  async findById(id) {
+    const result = await super.findById(id);
+
+    this.resolve(this.mapper.mapObject(result, answerDALtoDTO));
+  }
+
+  async find(condition) {
+    const result = await super.find(condition);
+
+    this.resolve(this.mapper.mapObject(result, answerDALtoDTO));
   }
 
   async addVote(userId, questionId) {
